7222d47009dd0212540db60e25922ae1cdd34689
Otavio Braga <otavio.chadbraga@gmail.com>
2025-03-19 15:59:54 -0300

    chore: removed apikey from ud instance

--- src/services/ud/index.ts
+++ src/services/ud/index.ts
@@ -4,8 +4,5 @@ export const udInstance = axios.create({
   baseURL: process.env.UD_API_URL,
   headers: {
     "Content-Type": "application/json",
-    apiKey: process.env.UD_API_KEY,
   },
 });
-
-udInstance.interceptors.response.use((response) => response.data);
